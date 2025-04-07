fb97c28e53c6ba73e84c0edad1d32d1c88ee9c24
Behrouz Sanaei Far <HOMAHOMAPAKRAVAN@GMAIL.COM>
2025-04-07 18:02:12 +0330

    Update index.js
    lip_D4UiF4a6I0nxj5rgn3HN

--- example/oauth-personal-token/index.js
+++ example/oauth-personal-token/index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const headers = {
-  Authorization: 'Bearer ' + process.env.lichessToken,
+  Authorization: 'Bearer ' + process.env.lip_D4UiF4a6I0nxj5rgn3HN
+,
 };
 
 fetch('https://lichess.org/api/account', { headers })
